90933efe6f15a74790a8695e89d01c88923cf0f0
Mounch-62 <j.saudemont.62@gmail.com>
2023-03-28 13:51:44 +0200

    design modif

--- page/profil/modifprofil.php
+++ page/profil/modifprofil.php
@@ -1,70 +1,53 @@
 <?php
-if (isset($_SESSION["login"])){
+if (isset($_SESSION["login"])) {
   $mysqlConnection = new PDO(
-  'mysql:host='.SERVER.';dbname='.DBNAME.';charset=utf8', 
-
-  USER, 
-
-  PASSWORD,
-    
-  [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION],
-);
-
+    'mysql:host=' . SERVER . ';dbname=' . DBNAME . ';charset=utf8',
+    USER,
+    PASSWORD,
+    [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION],
+  );
   // ordre de mission
   $requete = $mysqlConnection->prepare('SELECT * FROM user where id_user =:id');
   //execution de la requete
-  $requete->execute(["id"=>$_GET["id"]]);
+  $requete->execute(["id" => $_GET["id"]]);
 
   $user = $requete->fetch();
   $mysqlConnection = null;
   $requete = null;
-    ?>
-     <div class="row">
-        <div class="col center">
-          <form action="index.php?route=update_id&id=<?= $_GET["id"]?>&modif=1" method="post">
-            <div class="form-group">
-              <label for="login">login</label>
-              <input type="text" class="form-control" id="login" name="login" value=""/>
-            </div>         
-            <button type="submit" class="btn btn-primary">Modifer login</button>
-          </form>
-          <form action="index.php?route=update_id&id=<?= $_GET["id"]?>&modif=2" method="post">
-            <div class="form-group">
-              <label for="mail">mail</label>
-              <input type="text" class="form-control" id="mail" name="mail" value=""/>
-            </div>         
-            <button type="submit" class="btn btn-primary">Modifer mail</button>
-          </form>
-
-        </div>
-      </div>
-
-    <?php
-}
-else
-{
-    $_SESSION["error"]="il faut être connecté pour avoir acces";
-    echo"<script>window.location.href='index.php?route=login'</script>";
+} else {
+  $_SESSION["error"] = "il faut être connecté pour avoir acces";
+  header("location:index.php?route=welcome");
 }
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.3/flowbite.min.css" rel="stylesheet" />
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.3/flowbite.min.js"></script>
-    <script src="https://cdn.tailwindcss.com"></script>
-    <link href="index.css" rel="stylesheet"/>
-</head>
-<body>
-    <center><div id="color" class="p2"><button class="mon-bouton">Changer photo de profil</button>
-                                       <button class="bouton">Changer addresse Mail</button>
-                                       <button class="tbouton">Changer mot de passe</button>
-    </div></center>
-</body>
-    
+<p>
 
-</html>
+  <body class="bg-neutral-900">
+  <div class="text-xl text-white flex justify-center text mt-20 text-4xl">MODIFICATION DU PROFIL</div>
+      <div class="sm:max-w-md mt-6 px- py-4 rounded-lg bg-neutral-800 flex flex-col justify-center items-center w-full mx-auto shadow">
+        <form method="post" action="index.php?route=update_id&id=<?= $_GET["id"] ?>&modif=1">
+          <div class="mb-6">
+            <label for="id" class="block mb-2 text-sm font-medium text-fuchsia-700 ">Modifier le login</label>
+            <input type="text" name="login"
+              class="bg-gray-50 mb-2 border border-gray-300 text-fuchsia-700 text-sm rounded-lg focus:ring-fuchsia-700 focus:border-fuchsia-700 block w-full p-2.5 "
+              placeholder="nouveau login">
+            <button type="submit"
+              class="text-white bg-fuchsia-900 shadow-lg hover:bg-fuchsia-700 hover:scale-105 focus:ring-4 focus:outline-none focus:ring-fuchsia-900 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">
+              Modifier</button>
+          </div>
+        </form>
+
+        <form method="post" action="index.php?route=update_id&id=<?= $_GET["id"] ?>&modif=2">
+          <div class="mb-6">
+            <label for="mail" class="block mb-2 text-sm font-medium text-fuchsia-700">Modifier le mail</label>
+            <input type="text" name="mail"
+              class="bg-gray-50 border mb-2 border-gray-300 text-fuchsia-700 text-sm rounded-lg focus:ring-fuchsia-700 focus:border-fuchsia-700 block w-full p-2.5 "
+              placeholder="nouveau mail">
+            <button type="submit"
+              class="text-white bg-fuchsia-900 shadow-lg hover:bg-fuchsia-700 hover:scale-105 focus:ring-4 focus:outline-none focus:ring-fuchsia-900 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">
+              Modifier</button>
+          </div>
+        </form>
+      </div>
+</p>
+
+</body>
