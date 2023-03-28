28d0eabdaddd823099b1b7b8c0a61364ef9e4489
Yohanlng <yohan.languille@gmail.com>
2023-03-28 16:22:18 +0200

    changement database.php

--- config/database.php
+++ config/database.php
@@ -1,6 +1,6 @@
 <?php
-define("SERVER", "localhost");
-define("DBNAME", "unicorp");
-define("USER", "root");
-define("PASSWORD", "");
+define("SERVER", "172.20.8.111");
+define("DBNAME", "unicorp_bd");
+define("USER", "userdev");
+define("PASSWORD", "password");
 ?>
