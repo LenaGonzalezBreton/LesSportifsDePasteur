74aeebd4edb455fb5997dd1083396c150632b49c
LenaGB <lena.gonzalezbreton@proton.me>
2023-03-28 15:26:40 +0200

    Modification page welcome 28/03/2023

---
+++ common/footer.php
@@ -0,0 +1,3 @@
+<link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.3/flowbite.min.css" rel="stylesheet" />
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.3/flowbite.min.js"></script>
+        <script src="https://cdn.tailwindcss.com"></script>
