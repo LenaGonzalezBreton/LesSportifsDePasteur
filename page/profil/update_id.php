2654a1bd2d0ccfd689ef662fbc08b7bb72d36b68
LenaGB <lena.gonzalezbreton@proton.me>
2023-05-02 16:08:49 +0200

    modif mdp add seance

--- page/profil/update_id.php
+++ page/profil/update_id.php
@@ -60,9 +60,11 @@ if (isset($_GET["modif"])) {
             if (empty($_POST["old_pwd"])) {
                 $_SESSION["error"] = "Le mot de passe actuel est obligatoire";
                 echo "<script>window.location.href='index.php?route=modifprofil&id=" . $_GET["id"] . "'</script>";
+
             } else if (strlen($_POST["new_pwd"]) < 8) {
                 $_SESSION["error"] = "Le nouveau mot de passe doit contenir au moins 8 caractères";
                 echo "<script>window.location.href='index.php?route=modifprofil&id=" . $_GET["id"] . "'</script>";
+                
             } else if ($_POST["new_pwd"] !== $_POST["confirm_pwd"]) {
                 $_SESSION["error"] = "Les deux mots de passe ne correspondent pas";
                 echo "<script>window.location.href='index.php?route=modifprofil&id=" . $_GET["id"] . "'</script>";
@@ -78,45 +80,34 @@ if (isset($_GET["modif"])) {
                 $requete = $mysqlConnection->prepare('SELECT pwd FROM user WHERE id_user = :id');
                 $requete->execute(["id" => $_GET["id"]]);
                 $resultat = $requete->fetch(PDO::FETCH_ASSOC);
-
-                if (!password_verify($_POST["old_pwd"], $resultat["pwd"])) {
-                    $_SESSION["error"] = "Le mot de passe actuel est incorrect";
-                    echo "<script>window.location.href='index.php?route=modifprofil&id=" . $_GET["id"] . "'</script>";
+                if (strlen($resultat["pwd"]) < 60) {
+                    // Le mot de passe n'est pas chiffré
+                    if ($_POST["old_pwd"] !== $resultat["pwd"]) {
+                        // Le mot de passe actuel est incorrect
+                        $_SESSION["error"] = "Le mot de passe actuel est incorrect";
+                        echo "<script>window.location.href='index.php?route=modifprofil&id=" . $_GET["id"] . "'</script>";
+                    }
                 } else {
-
-                    // Mise à jour du mot de passe
-                    $password = $_POST["new_pwd"];
-                    $passwordHash = password_hash($password, PASSWORD_DEFAULT);
-                    $requete = $mysqlConnection->prepare('UPDATE user SET pwd = :new_pwd WHERE id_user = :id');
-                    $requete->execute(["id" => $_GET["id"], "new_pwd" => $passwordHash]);
-                    $mysqlConnection = null;
-                    $requete = null;
-                    $_SESSION["success"] = "Mot de passe modifié avec succès";
-                    echo "<script>window.location.href='index.php?route=welcome&id=" . $_GET["id"] . "'</script>";
+                    // Le mot de passe est chiffré
+                    if (!password_verify($_POST["old_pwd"], $resultat["pwd"])) {
+                        // Le mot de passe actuel est incorrect
+                        $_SESSION["error"] = "Le mot de passe actuel est incorrect";
+                        echo "<script>window.location.href='index.php?route=modifprofil&id=" . $_GET["id"] . "'</script>";
+                    } else {
+                        // Mise à jour du mot de passe
+                        $password = $_POST["new_pwd"];
+                        $passwordHash = password_hash($password, PASSWORD_DEFAULT);
+                        $requete = $mysqlConnection->prepare('UPDATE user SET pwd = :new_pwd WHERE id_user = :id');
+                        $requete->execute(["id" => $_GET["id"], "new_pwd" => $passwordHash]);
+                        $mysqlConnection = null;
+                        $requete = null;
+                        $_SESSION["success"] = "Mot de passe modifié avec succès";
+                        echo "<script>window.location.href='index.php?route=welcome&id=" . $_GET["id"] . "'</script>";
+                    }
                 }
             }
         } else if ($_GET["modif"] == "4") {
             if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
-                //     // Vérifie si un fichier a été téléchargé
-                //    // if (isset($_FILES["photo"]) && $_FILES["photo"]["error"] == 0) {
-                //         // Ouvre une connexion à la base de données
-                //         $mysqlConnection = new PDO(
-                //             'mysql:host=' . SERVER . ';dbname=' . DBNAME . ';charset=utf8',
-                //             USER,
-                //             PASSWORD,
-                //             [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION],
-                //         );
-
-                //         // Récupère le contenu de l'image et le convertit en base64
-                //         var_dump($_FILES["photo"]);
-                //         $imgData = $_FILES["photo"];
-                //         $base64Img = base64_encode($imgData);
-
-
-
-                //    // }
-
                 $mysqlConnection = new PDO(
                     'mysql:host=' . SERVER . ';dbname=' . DBNAME . ';charset=utf8',
                     USER,
@@ -134,17 +125,17 @@ if (isset($_GET["modif"])) {
                     if ($fileExt == 'jpg' || $fileExt == 'jpeg' || $fileExt == 'png' || $fileExt == 'gif') {
                         // Récupération du contenu du fichier
                         $fileContent = file_get_contents($_FILES['photo']['tmp_name']);
-                        
+
                         // Création d'une image à partir du contenu du fichier
                         $image = imagecreatefromstring($fileContent);
-                    
+
                         // Calcul des dimensions de l'image
                         $width = imagesx($image);
                         $height = imagesy($image);
-                    
+
                         // Récupération de la plus petite dimension pour couper l'image
                         $size = min($width, $height);
-                    
+
                         // Création d'une nouvelle image carrée
                         $croppedImage = imagecrop($image, [
                             'x' => ($width - $size) / 2,
@@ -152,21 +143,21 @@ if (isset($_GET["modif"])) {
                             'width' => $size,
                             'height' => $size
                         ]);
-                    
+
                         // Conversion de l'image en base64
                         ob_start();
                         imagepng($croppedImage);
                         $croppedContent = ob_get_clean();
                         $base64Image = base64_encode($croppedContent);
-                        
+
                         // Prépare et exécute la requête SQL pour stocker la chaîne de base64 dans la base de données
                         $requete = $mysqlConnection->prepare("UPDATE user SET photos = :photo WHERE id_user = :id");
                         $requete->execute(["id" => $_GET["id"], "photo" => "data:image/png;base64, " . $base64Image]);
-                    
+
                         $_SESSION["success"] = "Image stockée dans la base de données avec succès.";
                         echo "<script>window.location.href='index.php?route=welcome&id=" . $_GET["id"] . "'</script>";
-                    
-                    
+
+
                     } else {
                         echo 'Le fichier doit être une image de type jpg, jpeg, png ou gif.';
                     }
--- page/seance/addSeance.php
+++ page/seance/addSeance.php
@@ -1,5 +1,5 @@
 <?php
-
+//Connexion à la base de données
 if (isset($_SESSION["login"])) {
   $mysqlConnection = new PDO(
     'mysql:host=' . SERVER . ';dbname=' . DBNAME . ';charset=utf8',
@@ -10,60 +10,92 @@ if (isset($_SESSION["login"])) {
 
     [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION],
   );
- // ordre de mission
- $requete = $mysqlConnection->prepare('SELECT * FROM sport');
- //execution de la requete
- $requete->execute();
+  // ordre de mission
+  $requete = $mysqlConnection->prepare('SELECT * FROM sport');
+  //execution de la requete
+  $requete->execute();
 
- $sports = $requete->fetchAll();
- $requete = null;
- $requete2 = $mysqlConnection->prepare('SELECT * FROM user WHERE id_user=:id');
- $requete2->execute([
-     'id'=>$_GET["id"]
- ]);
+  $sports = $requete->fetchAll();
+  $requete = null;
+  $requete2 = $mysqlConnection->prepare('SELECT * FROM user WHERE id_user=:id');
+  $requete2->execute([
+    'id' => $_GET["id"]
+  ]);
 
- $user= $requete2->fetch();
- $_SESSION["id"]=$user["id_user"];
-?>
-  <div>
-    <div class="col center">
-      <form action=<?php echo"'index.php?route=insertSeance&id=".$_SESSION["id"]."'"; ?>, method="post">
+  $user = $requete2->fetch();
+  $_SESSION["id"] = $user["id_user"];
+  ?>
+
+  <!--HTML CSS de la page -->
+  <div class="grid grid-cols-2 gap-8">
+    <div
+      class="sm:max-w-md mt-6 px- py-4 rounded-lg bg-neutral-800 flex flex-col justify-center items-center w-full mx-auto shadow">
+      <form action=<?php echo "'index.php?route=insertSeance&id=" . $_SESSION["id"] . "'"; ?>, method="post">
         <div class="form-group">
-          <label for="seance">Séances</label>
-          <input type="text" class="form-control" id="seance" name="seance" placeholder="Entrer une séance">
+          <label for="seance" class="block mb-2 text-sm font-medium text-fuchsia-700 ">Séances</label>
+          <input type="text" class="form-control rounded-lg" id="seance" name="seance" placeholder="Entrer une séance">
         </div>
         <div class="form-group">
-          <label for="dte">date</label>
-          <input type="date" class="form-control" id="dte" name="dte" placeholder="Entrer une date">
+          <label for="dte" class="block mb-2 text-sm font-medium text-fuchsia-700 ">Date</label>
+          <input type="date" class="form-control rounded-lg" id="dte" name="dte" placeholder="Entrer une date">
         </div>
         <div class="form-group">
-          <label for="hDebut">heure debut</label>
-          <input type="time" class="form-control" id="hDebut" name="hDebut" placeholder="Entrer une heure">
+          <label for="hDebut" class="block mb-2 text-sm font-medium text-fuchsia-700 ">Heure début</label>
+          <input type="time" class="form-control rounded-lg" id="hDebut" name="hDebut" placeholder="Entrer une heure">
         </div>
         <div class="form-group">
-          <label for="hFin">heure fin</label>
-          <input type="time" class="form-control" id="hFin" name="hFin" placeholder="Entrer une heure">
+          <label for="hFin" class="block mb-2 text-sm font-medium text-fuchsia-700 ">Heure fin</label>
+          <input type="time" class="form-control rounded-lg" id="hFin" name="hFin" placeholder="Entrer une heure">
         </div>
         <div class="form-group">
-              <label for="sports">Sports</label>
-              <select class="form-control" id="sports" name="sports" placeholder="Sélectionner un sport">
-                  <option value="" disabled selected hidden>Choisir un sport</option>
-                  <?php
-                  foreach ($sports as $ligne){
-                      ?>
-                      <option value="<?= $ligne["id_sports"] ?>" ><?= $ligne["nom_sports"]?></option>
-                  <?php
-                  }
-                  ?>
-              </select>
-            </div>           
-        <button type="submit" class="btn btn-primary">Créer une séance</button>
+          <label for="sports" class="block mb-2 text-sm font-medium text-fuchsia-700 ">Sports</label>
+          <select class="form-control rounded-lg" id="sports" name="sports" placeholder="Sélectionner un sport">
+            <option value="" disabled selected hidden>Choisir un sport</option>
+            <?php
+            foreach ($sports as $ligne) {
+              ?>
+              <option value="<?= $ligne["id_sports"] ?>"><?= $ligne["nom_sports"] ?></option>
+              <?php
+            }
+            ?>
+          </select>
+        </div>
+        <button type="submit" class="btn btn-primary text-white bg-fuchsia-900 shadow-lg hover:bg-fuchsia-700 hover:scale-105 focus:ring-4 focus:outline-none focus:ring-fuchsia-900 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">Créer
+          une séance</button>
       </form>
-
     </div>
-  </div>
+    <div class="sm:max-w-md mt-6 px- py-4 rounded-lg bg-neutral-800 flex flex-col justify-center items-center w-full mx-auto shadow">
+      <label for="id" class="block mb-2 text-sm font-medium text-fuchsia-700 ">Tableau séances</label>
+      <table class="w-full text-sm text-left text-gray-500 bg-neutral-800">
+        <tbody>
+          <?php
+         $res= $mysqlConnection->query("SELECT * FROM seance");
+         $seances=$res->fetchAll(); 
+          foreach ($seances as $ligne) {
+            ?>
+            <tr class="bg-white border-b ">
+              <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
+                <?= $ligne["dte_seance"] ?>
+              </th>
+              <td class="font-medium text-blue-600 ">
+                <a href="index.php?route=delete-atelier&id=<?= $ligne["id_seance"] ?>"><button
+                    class="btn btn-danger text-fuchsia-700">Supprimer</button></a>
+              <td>
+                <a href="index.php?route=edit-atelier&id=<?= $ligne["id_seance"] ?>"><button
+                    class="btn btn-info">Modifier</button></a>
+              </td>
 
-<?php
+            </tr>
+            <?php
+          }
+          ?>
+          </tbody>
+        </table>
+      </div>
+    </div>
+
+    <?php
+          
 } else {
   $_SESSION["error"] = "il faut être connecté pour avoir acces";
   header("location:./index.php");
