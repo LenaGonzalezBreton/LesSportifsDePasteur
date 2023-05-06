f664f1e64e520f19e936cb4a3ee649028598c32e
Bekso1204 <alexian.pellier2912@gmail.com>
2023-05-06 18:10:00 +0200

    creer seances

--- page/seance/insertSeance.php
+++ page/seance/insertSeance.php
@@ -1,8 +1,9 @@
 <?php
+
 if (isset($_POST["seance"])==false || empty($_POST["seance"]) || isset($_POST["sports"])==false || empty($_POST["sports"])){
 
     $_SESSION["error"]="Le sport et la séance sont obligatoires";
-    echo"<script>window.location.href='index.php?route=addSeance'</script>";
+    echo"<script>window.location.href='index.php?route=addSeance&id=".$_GET["id"]."'</script>";
 }
 else
 {
@@ -14,14 +15,19 @@ else
     ); 
 
 // ordre de mission
-$requete = $mysqlConnection->prepare('INSERT INTO seance(libelle_seance,fk_id_sport, dte_seance, heure_debut, heure_fin) values(:seance,:fk_id_sports,:dte,:hDebut,:hFin)');
+$requete = $mysqlConnection->prepare('INSERT INTO seance(libelle_seance, dte_seance, heure_debut, heure_fin) values(:seance,:dte,:hDebut,:hFin)');
 //execution de la requete
-$requete->execute( ["seance"=>$_POST["seance"],"fk_id_sports"=>$_POST["sports"], "dte"=>$_POST["dte"],"hDebut"=>$_POST["hDebut"], "hFin"=>$_POST["hFin"]]);
-$requete = null;
-$requete2 = $mysqlConnection->prepare('SELECT * FROM user WHERE id_user=:id');
-$requete2->execute(['id'=>$_GET["id"]]);
-$user=$requete2->fetch();
-$_SESSION["id"]=$user["id_user"];
+$requete->execute( ["seance"=>$_POST["seance"], "dte"=>$_POST["dte"],"hDebut"=>$_POST["hDebut"], "hFin"=>$_POST["hFin"]]);
+
+$lastId = $mysqlConnection->lastInsertId();
+$intLastId=(int)$lastId;
+
+foreach($_POST["sports"] as $ligne){
+    $requete3=$mysqlConnection->prepare('INSERT INTO sport_seance(fk_id_seance, fk_id_sport) VALUES (:id_seance, :id_sport)');
+    $requete3->execute(["id_seance"=>$intLastId, "id_sport"=>$ligne]);
+    $requete3 = null;
+}
+
 $_SESSION["success"]="Séance crée avec succès";
-echo"<script>window.location.href='index.php?route=welcome&id=".$_SESSION["id"]."'</script>";
+echo"<script>window.location.href='index.php?route=welcome&id=".$_GET["id"]."'</script>";
 }
--- page/sport/insertSport.php
+++ page/sport/insertSport.php
@@ -14,21 +14,16 @@ if (isset($_POST["sports"]) == false || empty($_POST["sports"])) {
     $requete = $mysqlConnection->prepare('SELECT * FROM sport WHERE nom_sports=:sport');
     $requete->execute(['sport' => $_POST["sports"]]);
     $sports = $requete->fetch();
-
-    $requete2 = $mysqlConnection->prepare('SELECT * FROM user WHERE id_user=:id');
-    $requete2->execute(['id' => $_GET["id"]]);
-    $user = $requete2->fetch();
-    $_SESSION["id"] = $user["id_user"];
 }
 
         if ($sports) {
             $_SESSION["doublon"] = "Sport existe déjà";
-            echo "<script>window.location.href='index.php?route=addSport&id=" . $_SESSION["id"] . "'</script>";
+            echo "<script>window.location.href='index.php?route=addSport&id=" . $_GET["id"] . "'</script>";
         } else {
             $requete3 = $mysqlConnection->prepare('INSERT INTO sport (nom_sports) values(:sports)');
             $requete3->execute(["sports" => $_POST["sports"]]);
             $insertSport = $requete3->fetch();
             $_SESSION["success"] = "Sport créé avec succès";
-            echo "<script>window.location.href='index.php?route=welcome&id=" . $_SESSION["id"] . "'</script>";
+            echo "<script>window.location.href='index.php?route=welcome&id=" . $_GET["id"] . "'</script>";
         }
 ?>
